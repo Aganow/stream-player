ee764e616165a5a74a9a3191a8ac77df09907d7f
Jan Cipher <55353943+Aganow@users.noreply.github.com>
2023-06-19 16:46:30 +0500

    Add files via upload

---
+++ audio_player.py
@@ -0,0 +1,51 @@
+import requests
+import vlc
+
+
+class StreamPlayer:
+    def __init__(self, url):
+        self.url = url
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
+        self.media = self.vlc_instance.media_new(url)
+        self.player.set_media(self.media)
+
+    def play(self):
+        try:
+            self.player.play()
+            input("Press Enter to stop playback...")
+            self.player.stop()
+        except Exception as e:
+            print(f"An error occurred while playing the stream: {e}")
+        finally:
+            self.player.release()
+
+
+class StreamInfo:
+    def __init__(self, url):
+        self.url = url
+
+    def get_info(self):
+        try:
+            response = requests.head(self.url)
+            if response.status_code == 200:
+                print("Stream information:")
+                for key, value in response.headers.items():
+                    print(f"{key}: {value}")
+            else:
+                print("Failed to connect to the stream.")
+        except requests.RequestException as e:
+            print(f"An error occurred while retrieving stream information: {e}")
+
+
+def main():
+    stream_url = "http://c2.radioboss.fm:8734/stream"
+    
+    info = StreamInfo(stream_url)
+    info.get_info()
+    
+    player = StreamPlayer(stream_url)
+    player.play()
+
+if __name__ == "__main__":
+    main()
